a6111ce9f9c96b5c51f263d782f42ffe840b6304
Nathan Glick <nateleighglick@hotmail.com>
2020-11-03 10:13:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,11 @@ def long_planeteer_calls(long_call)
     answer = true 
   end
  end
- answer
+  answer
  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-end
+
